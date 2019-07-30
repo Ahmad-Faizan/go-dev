79e1b4eb850e7380ee03ee9dadf757bd1945a5ea
Faizan Ahmad <faizan.nasro@gmail.com>
2019-07-30 19:53:29 +0530

    Handled edge cases for unsavoury inputs

--- src/calculator/calc.go
+++ src/calculator/calc.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"math"
+	"os"
 )
 
 func add(x, y float64) float64 {
@@ -24,11 +25,17 @@ func div(x, y float64) float64 {
 func main() {
 	var a, b float64
 	var opr string
+
 	fmt.Println("Enter the operation (+ , - , * , / ) ")
 	fmt.Scan(&opr)
-	fmt.Println("Enter the two operands ")
-	fmt.Scan(&a, &b)
 
+	fmt.Println("Enter the two operands ")
+
+	_, err := fmt.Scan(&a, &b)
+	if err != nil {
+		fmt.Println("Invalid number format", err)
+		os.Exit(1)
+	}
 	switch opr {
 	case "+":
 		fmt.Printf("Result of %v operation : %.3f", opr, add(a, b))
@@ -38,5 +45,7 @@ func main() {
 		fmt.Printf("Result of %v operation : %.3f", opr, mul(a, b))
 	case "/":
 		fmt.Printf("Result of %v operation : %.3f", opr, div(a, b))
+	default:
+		fmt.Println("Invalid Operator specified")
 	}
 }
